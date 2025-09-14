157de1c9b5175c2cf638c191d8629e5a9fed00f8
Max Rietmeijer <max@bouwers.dev>
2025-09-14 14:18:57 +0200

    Added mongo interaction

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_req, res) => res.json({}));
+const client = new MongoClient(process.env.MONGODB_URI);
 
-app.listen(PORT, () => {
-    console.log(`API listening on :${PORT}`);
-});
+(async () => {
+    await client.connect();
+
+    const db = client.db("cheerstag");
+    const collection = db.collection("qr");
+
+    app.get("/", async (_req, res) => {
+        const docs = await collection.find().toArray();
+
+        return res.json(docs);
+    });
+
+    app.listen(PORT, () => {
+        console.log(`API listening on :${PORT}`);
+    });
+})();
